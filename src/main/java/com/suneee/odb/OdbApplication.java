43d285714f50c5a3527a44e6a0876eff713eb95c
ChenBo <hopetilight@outlook.com>
2018-12-14 17:11:43 +0800

    [ADD] create projet OBD

---
+++ src/main/java/com/suneee/odb/OdbApplication.java
@@ -0,0 +1,14 @@
+package com.suneee.odb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdbApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (OdbApplication.class, args);
+    }
+
+}
+
